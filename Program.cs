181494875172c844dc6e1ea3590bdde6fb74a3dd
Vera Kalinkin <vera-kalinkina11@rambler.ru>
2023-12-23 16:48:32 +0400

    prog

--- Program.cs
+++ Program.cs
@@ -1,1 +1,13 @@
-﻿Console.WriteLine("Hello, World!");
+﻿//Console.WriteLine("Hello, World!");
+
+using System.Diagnostics.CodeAnalysis;
+
+int count = 193728;
+while (count > 0)
+{
+    int a = count % 10;
+    count = count / 10;
+    Console.Write(a);
+    Console.Write(' ');
+};
+
